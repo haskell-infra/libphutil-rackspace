6c99df9afbdf0ec244ed139250468bb7f1a70cfc
Austin Seipp <aseipp@pobox.com>
2014-08-12 23:06:06 -0500

    Merge configuration options.
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- src/extensions/config/option/PhabricatorRackspaceConfigOptions.php
+++ src/extensions/config/option/PhabricatorRackspaceConfigOptions.php
@@ -23,6 +23,29 @@ final class PhabricatorRackspaceConfigOptions
         ->setDescription(pht(
           'Set this to true to enable access logs for all data that the file '.
           'objects acrue.')),
+      $this->newOption('storage.rackspace.container', 'string', null)
+         ->setSummary(pht('Rackspace Files container.'))
+         ->setDescription(
+           pht(
+             "Set this to a valid Rackspace Files container to store files ".
+             "there. You must also configure the Rackspace access keys in the ".
+             "'Rackspace Cloud' group, and the region to store the files in.")),
+      $this->newOption('storage.rackspace.region', 'enum', null)
+         ->setEnumOptions(
+           array(
+             'IAD' => 'Northern Virginia (IAD)',
+             'ORD' => 'Chicago (ORD)',
+             'SYD' => 'Sydney (SYD)',
+             'DFW' => 'Dallas (DFW)',
+             'LON' => 'London (LON)',
+             'HKG' => 'Hong Kong (HKG)',
+           ))
+         ->setSummary(pht('Rackspace Files region.'))
+         ->setDescription(
+           pht(
+             "Set this to a valid Rackspace region, which specifies which ".
+             "region your Cloud Files will be stored in. The default is ".
+             "`null`.")),
     );
   }
 
