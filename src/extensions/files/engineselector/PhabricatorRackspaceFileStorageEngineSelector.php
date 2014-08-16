5ef75ce43686de0cfcf387225424e627677037e6
Austin Seipp <aseipp@pobox.com>
2014-08-16 00:49:03 -0500

    files: *Only* use Rackspace in the Rackspace EngineSelctor
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- src/extensions/files/engineselector/PhabricatorRackspaceFileStorageEngineSelector.php
+++ src/extensions/files/engineselector/PhabricatorRackspaceFileStorageEngineSelector.php
@@ -11,25 +11,8 @@ final class PhabricatorRackspaceFileStorageEngineSelector
    * Select viable default storage engine.
    */
   public function selectStorageEngines($data, array $params) {
-    $length = strlen($data);
-
-    $mysql_key = 'storage.mysql-engine.max-size';
-    $mysql_limit = PhabricatorEnv::getEnvConfig($mysql_key);
-
     $engines = array();
-    /* First: MySQL if allowed */
-    if ($mysql_limit && $length <= $mysql_limit) {
-      $engines[] = new PhabricatorMySQLFileStorageEngine();
-    }
 
-    /* -- Check local disk -- */
-    $local_key = 'storage.local-disk.path';
-    $local_path = PhabricatorEnv::getEnvConfig($local_key);
-    if ($local_path) {
-      $engines[] = new PhabricatorLocalDiskFileStorageEngine();
-    }
-
-    /* -- Select Rackspace before S3 -- */
     $rackspace_key       = 'rackspace-files.container';
     $rackspace_container = PhabricatorEnv::getEnvConfig($rackspace_key);
 
@@ -38,26 +21,13 @@ final class PhabricatorRackspaceFileStorageEngineSelector
 
     if ($rackspace_container && $rackspace_region) {
       $engines[] = new PhabricatorRackspaceFileStorageEngine();
+      return $engines;
     }
 
-    /* -- Check S3 -- */
-    $s3_key = 'storage.s3.bucket';
-    if (PhabricatorEnv::getEnvConfig($s3_key)) {
-      $engines[] = new PhabricatorS3FileStorageEngine();
-    }
-
-    if ($mysql_limit && empty($engines)) {
-      // If we return no engines, an exception will be thrown but it will be
-      // a little vague ("No valid storage engines"). Since this is a default
-      // case, throw a more specific exception.
-      throw new Exception(
-        'This file exceeds the configured MySQL storage engine filesize '.
-        'limit, but no other storage engines are configured. Increase the '.
-        'MySQL storage engine limit or configure a storage engine suitable '.
-        'for larger files.');
-    }
-
-    return $engines;
+    throw new Exception(
+      'You selected the Rackspace file storage engine, but have not '.
+      'configured a container or region to use yet. Please do so '.
+      'immediately under `Config > Rackspace`.');
   }
 }
 
